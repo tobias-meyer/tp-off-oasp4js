4de322b95abbe095e1b63e354e9d0e65810a482d
Bastian Ramm <bastian.ramm@capgemini.com>
2015-06-23 19:01:32 +0200

    Added mock data for specials

--- app/special-mgmt/js/table-search.controller.js
+++ app/special-mgmt/js/table-search.controller.js
@@ -1,106 +1,150 @@
+// TODO remove mocking
+var mockData_specials = [
+	{
+		"id": 101,
+		"modificationCounter": 1,
+		"revision": null,
+		"waiterId": null,
+		"number": 1,
+		"state": "OCCUPIED"
+	},
+	{
+		"id": 102,
+		"modificationCounter": 1,
+		"revision": null,
+		"waiterId": null,
+		"number": 2,
+		"state": "FREE"
+	},
+	{
+		"id": 103,
+		"modificationCounter": 1,
+		"revision": null,
+		"waiterId": null,
+		"number": 3,
+		"state": "FREE"
+	},
+	{
+		"id": 104,
+		"modificationCounter": 1,
+		"revision": null,
+		"waiterId": null,
+		"number": 4,
+		"state": "ICH BIN ES"
+	}
+];
+
 angular.module('app.special-mgmt')
     .controller('SpecialSearchCntl', function ($scope, tables, paginatedTableList, $modal, globalSpinner, offers, sales) {
-        'use strict';
-        var selectedTable = function () {
-            return $scope.selectedItems && $scope.selectedItems.length ? $scope.selectedItems[0] : undefined;
-        };
-        
-        $scope.openEditDialog = function (tableRow) {
-            $modal.open({
-                templateUrl: 'special-mgmt/html/table-details.html',
-                controller: 'TableDetailsCntl',
-                resolve: {
-                    tableDetails: function () {
-                        return tables.loadTable(tableRow.id);
-                    },
-                    allOffers: function () {
-                        return offers.loadAllOffers();
-                    },
-                    currentOrder: function () {
-                        return sales.loadOrderForTable(tableRow.id);
-                    }
-                }
-            });
-        };
-    
-        $scope.selectedItems = [];
-        $scope.maxSize = 5;
-        $scope.totalItems = paginatedTableList.pagination.total;
-        $scope.numPerPage = paginatedTableList.pagination.size;
-        $scope.currentPage = paginatedTableList.pagination.page;
+	'use strict';
+	var selectedSpecial = function () {
+		return $scope.selectedItems && $scope.selectedItems.length ? $scope.selectedItems[0] : undefined;
+	};
 
-        $scope.gridOptions = {
-            data: paginatedTableList.result
-        };
-        
-        $scope.reloadTables = function () {
-            tables.getPaginatedTables($scope.currentPage, $scope.numPerPage).then(function (paginatedTables) {
-                return paginatedTables;
-            }).then(function (res) {
-                paginatedTableList = res;
-                $scope.gridOptions.data = paginatedTableList.result;
-            });
-        };
-        
-        $scope.$watch('currentPage', function () {
-            $scope.reloadTables();
-        });
-        
-        $scope.buttonDefs = [
-            {
-                label: 'Edit...',
-                onClick: function () {
-                    $scope.openEditDialog(selectedTable());
-                },
-                isActive: function () {
-                    return selectedTable();
-                }
-            },
-            {
-                label: 'Reserve',
-                onClick: function () {
-                    globalSpinner.decorateCallOfFunctionReturningPromise(function () {
-                        return tables.reserve(selectedTable()).then($scope.reloadTables);
-                    });
-                },
-                isActive: function () {
-                    return selectedTable() && selectedTable().state === 'FREE';
-                }
-            },
-            {
-                label: 'Cancel Reservation',
-                onClick: function () {
-                    globalSpinner.decorateCallOfFunctionReturningPromise(function () {
-                        return tables.cancelReservation(selectedTable()).then($scope.reloadTables);
-                    });
-                },
-                isActive: function () {
-                    return selectedTable() && selectedTable().state === 'RESERVED';
-                }
-            },
-            {
-                label: 'Occupy',
-                onClick: function () {
-                    globalSpinner.decorateCallOfFunctionReturningPromise(function () {
-                        return tables.occupy(selectedTable()).then($scope.reloadTables);
-                    });
-                },
-                isActive: function () {
-                    return selectedTable() && (selectedTable().state === 'RESERVED' || selectedTable().state === 'FREE');
-                }
-            },
-            {
-                label: 'Free',
-                onClick: function () {
-                    globalSpinner.decorateCallOfFunctionReturningPromise(function () {
-                        return tables.free(selectedTable()).then($scope.reloadTables);
-                    });
-                },
-                isActive: function () {
-                    return selectedTable() && selectedTable().state === 'OCCUPIED';
-                }
-            }
-        ];
-    
+	$scope.test = "Hallo Test: Special Controller!";
 
-    });
+	$scope.openEditDialog = function (specialRow) {
+		$modal.open({
+			templateUrl: 'special-mgmt/html/table-details.html',
+			controller: 'TableDetailsCntl',
+			resolve: {
+				tableDetails: function () {
+					return tables.loadTable(specialRow.id);
+				},
+				allOffers: function () {
+					return offers.loadAllOffers();
+				},
+				currentOrder: function () {
+					return sales.loadOrderForTable(specialRow.id);
+				}
+			}
+		});
+	};
+
+	$scope.selectedItems = [];
+	$scope.maxSize = 5;
+	$scope.totalItems = paginatedTableList.pagination.total;
+	$scope.numPerPage = paginatedTableList.pagination.size;
+	$scope.currentPage = paginatedTableList.pagination.page;
+
+
+	$scope.gridOptions = {
+		// TODO remove mocking
+        //     data: paginatedTableList.result
+		data: mockData_specials
+	};
+
+
+	$scope.reloadSpecials = function () {
+		tables.getPaginatedTables($scope.currentPage, $scope.numPerPage).then(function (paginatedTables) {
+			return paginatedTables;
+		}).then(function (res) {
+			paginatedTableList = res;
+			// TODO remove mocking
+			//$scope.gridOptions.data = paginatedTableList.result;
+			$scope.gridOptions.data = mockData_specials;
+		});
+	};
+
+	$scope.$watch('currentPage', function () {
+		$scope.reloadSpecials();
+	});
+
+	$scope.buttonDefs = [
+		{
+			label: 'Edit...',
+			onClick: function () {
+				$scope.openEditDialog(selectedSpecial());
+			},
+			isActive: function () {
+				return selectedSpecial();
+			}
+		},
+		{
+			label: 'Reserve',
+			onClick: function () {
+				globalSpinner.decorateCallOfFunctionReturningPromise(function () {
+					return tables.reserve(selectedSpecial()).then($scope.reloadSpecials);
+				});
+			},
+			isActive: function () {
+				return selectedSpecial() && selectedSpecial().state === 'FREE';
+			}
+		},
+		{
+			label: 'Cancel Reservation',
+			onClick: function () {
+				globalSpinner.decorateCallOfFunctionReturningPromise(function () {
+					return tables.cancelReservation(selectedSpecial()).then($scope.reloadSpecials);
+				});
+			},
+			isActive: function () {
+				return selectedSpecial() && selectedSpecial().state === 'RESERVED';
+			}
+		},
+		{
+			label: 'Occupy',
+			onClick: function () {
+				globalSpinner.decorateCallOfFunctionReturningPromise(function () {
+					return tables.occupy(selectedSpecial()).then($scope.reloadSpecials);
+				});
+			},
+			isActive: function () {
+				return selectedSpecial() && (selectedSpecial().state === 'RESERVED' || selectedSpecial().state === 'FREE');
+			}
+		},
+		{
+			label: 'Free',
+			onClick: function () {
+				globalSpinner.decorateCallOfFunctionReturningPromise(function () {
+					return tables.free(selectedSpecial()).then($scope.reloadSpecials);
+				});
+			},
+			isActive: function () {
+				return selectedSpecial() && selectedSpecial().state === 'OCCUPIED';
+			}
+		}
+	];
+
+
+});
