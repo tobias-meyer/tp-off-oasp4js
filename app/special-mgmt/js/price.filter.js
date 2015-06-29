aa6f5ade451fb6d80f6431f390f8cb8231a3fbd2
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2015-06-29 10:23:34 +0200

    Reverted to keep latest development state to start with on develoment branch
    Revert "Merge pull request #2 from cgbram/development"
    
    This reverts commit 25ff7307cdd84f8b69583bb78911c33f80c611b3, reversing
    changes made to c61fbf90390132da382a7b6aa40c89d01aa07e25.

--- app/special-mgmt/js/price.filter.js
+++
@@ -1,7 +0,0 @@
-angular.module('app.special-mgmt')
-    .filter('price', function () {
-        'use strict';
-        return function (item) {
-            return item + ' EUR';
-        };
-    });
